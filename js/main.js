7aa89454388b0c579c77e6e6058daf700136a070
Jim Aldrin <aldrinsotelo@gmail.com>
2026-09-15 23:39:02 +0800

    Banner completed, started working on add task dialog

--- js/main.js
+++ js/main.js
@@ -1,5 +1,54 @@
 import { renderCalendar } from './calendar.js';
 
+const focusRing = document.querySelector('#focus-ring');
+
+const progressPercentage = document.querySelector('#progress-percentage');
+
+const completedCount = document.querySelector('#completed-count');
+
+const totalCount = document.querySelector('#total-count');
+
 renderCalendar();
 
 lucide.createIcons();
+
+function updateProgress() {
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [
+    {
+      title: 'Code',
+      completed: false,
+    },
+    {
+      title: 'Vibe',
+      completed: true,
+    },
+    {
+      title: 'Vibe',
+      completed: true,
+    },
+  ];
+
+  const total = tasks.length;
+  const completed = tasks.filter((task) => task.completed).length;
+
+  let percentage;
+
+  if (total === 0) {
+    percentage = 0;
+  } else {
+    percentage = Math.round((completed / total) * 100);
+  }
+
+  focusRing.style.background = `
+    conic-gradient(
+    #ffffff 0 ${percentage}%,
+    rgba(255, 255, 255, 0.26) ${percentage}% 100%
+  )`;
+
+  progressPercentage.textContent = percentage;
+
+  completedCount.textContent = completed;
+  totalCount.textContent = total;
+}
+
+updateProgress();
